0c92e7d4bc4198345513c6d5c91cc5b7b34c2a71
ahmetoral3 <90841737+ahmetoral3@users.noreply.github.com>
2021-12-15 19:15:53 +0100

    Update main.cpp

--- testcode/main.cpp
+++ testcode/main.cpp
@@ -2,38 +2,44 @@
 
 int reed_switch = 21;
 int reed_status;
+
+const int trigPin = 22;
+const int echoPin = 23;
+
+const int trigPin1 = 18;
+const int echoPin1 = 19;
+#define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+long duration1;
+float distanceCm;
+float distanceCm1;
+
 void setup() {
-    pinMode(reed_switch, INPUT);
     Serial.begin(9600);
+    //reed
+    pinMode(reed_switch, INPUT);
+    //ultrasoon
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    pinMode(trigPin1, OUTPUT);
+    pinMode(echoPin1,INPUT);
+    //ir-sensor
+    pinMode(16,INPUT);
+    pinMode(17,INPUT);
   }
   void loop(){
+    //reed
     reed_status = digitalRead(reed_switch);
     if (reed_status == 1){
     Serial.println("0");
     }else{
     Serial.println("1");
     delay(1000);}
-    }
 
-/*const int trigPin = 27;
-const int echoPin = 26;
-
-//define sound speed in cm/uS
-#define SOUND_SPEED 0.034
-#define CM_TO_INCH 0.393701
-
-long duration;
-float distanceCm;
-float distanceInch;
-
-void setup() {
-  Serial.begin(9600); // Starts the serial communication
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-}
-
-void loop() {
-  // Clears the trigPin
+    // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -47,36 +53,43 @@ void loop() {
   // Calculate the distance
   distanceCm = duration * SOUND_SPEED/2;
   
-  // Convert to inches
-  distanceInch = distanceCm * CM_TO_INCH;
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+
+  duration1 = pulseIn(echoPin1, HIGH);
+  
+  // Calculate the distance
+  distanceCm1 = duration1 * SOUND_SPEED/2;
   
   // Prints the distance in the Serial Monitor
   Serial.print("Distance (cm): ");
   Serial.println(distanceCm);
-  Serial.print("Distance (inch): ");
-  Serial.println(distanceInch);
+  Serial.print("Distance1 (cm): ");
+  Serial.println(distanceCm1);
   
-  delay(1000);
-}*/
 
-/*void setup()
-{
-  pinMode(16,INPUT);
-  Serial.begin(9600);
-}
-void loop()
-{
+  //ir-sensor
   if (digitalRead(16)== LOW)
-  {
-    Serial.println("1");
-    
-    delay(100);
-  }
+    {
+      Serial.println("0 = 1");
+    }
   else 
-  {
-    
-    Serial.println("0");
-    
-  }
-  
-}*/
+    {
+      Serial.println("0 = 0");
+    }
+
+  if (digitalRead(17)== LOW)
+    {
+      Serial.println("1 = 1");
+    }
+  else 
+    {
+      Serial.println("1 = 0");
+    }
+
+    delay(1000);
+}
